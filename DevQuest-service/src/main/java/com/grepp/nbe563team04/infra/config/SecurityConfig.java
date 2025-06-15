9b9e982838e2e54518175766d1a8f2d1ec94e7dc
imseohyeon <forestlight749@gmail.com>
2025-06-16 00:02:35 +0900

    (0615) feat: #3 Spring Security 인증 방식을 세션에서 JWT 기반으로 전환

--- DevQuest-service/src/main/java/com/grepp/nbe563team04/infra/config/SecurityConfig.java
+++ DevQuest-service/src/main/java/com/grepp/nbe563team04/infra/config/SecurityConfig.java
@@ -1,103 +1,95 @@
 package com.grepp.nbe563team04.infra.config;
 
-import com.grepp.nbe563team04.model.member.MemberService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import com.grepp.nbe563team04.infra.auth.UserDetailsServiceImpl;
+import com.grepp.nbe563team04.infra.auth.token.filter.AuthExceptionFilter;
+import com.grepp.nbe563team04.infra.auth.token.filter.JwtAuthenticationFilter;
+import com.grepp.nbe563team04.infra.auth.token.filter.LogoutFilter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Slf4j
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Value("${remember-me.key}")
-    private String rememberMeKey;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthExceptionFilter authExceptionFilter;
+    private final LogoutFilter logoutFilter;
 
     @Bean
-    public AuthenticationSuccessHandler successHandler() {
-        return (request, response, authentication) -> {
+    public AuthenticationSuccessHandler successHandler(){
+        return new AuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request,
+                HttpServletResponse response, Authentication authentication)
+                throws IOException, ServletException {
 
-            boolean isAdmin = authentication.getAuthorities()
-                .stream()
-                .anyMatch(authority -> authority.getAuthority().equals("ROLE_ADMIN"));
+                boolean isAdmin = authentication.getAuthorities()
+                    .stream()
+                    .anyMatch(authority ->
+                        authority.getAuthority().equals("ROLE_ADMIN"));
 
-            if (isAdmin) {
-                response.sendRedirect("/admin/dashboard");
-                return;
-            }
-            response.sendRedirect("/dashboard");
-        };
-    }
-
-    @Bean
-    public AccessDeniedHandler accessDeniedHandler() {
-        return (request, response, accessDeniedException) -> {
-            Authentication auth = (Authentication) request.getUserPrincipal();
-
-            if (auth != null) {
-                boolean isAdmin = auth.getAuthorities().stream()
-                    .map(GrantedAuthority::getAuthority)
-                    .anyMatch(role -> role.equals("ROLE_ADMIN"));
-
-                if (isAdmin) {
-                    response.sendRedirect("/admin/dashboard");
-                } else {
-                    response.sendRedirect("/dashboard");
+                if(isAdmin){
+                    response.sendRedirect("/admin");
+                    return;
                 }
-            } else {
-                response.sendRedirect("/signin");
+
+                response.sendRedirect("/");
             }
         };
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, MemberService memberService)
-        throws Exception {
-        http.userDetailsService(memberService)
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/css/**", "/js/**", "/img/**", "/api/member/exists/*",
-                                "/member/interests").permitAll() // 모두에게 허용
-                        .requestMatchers("/", "/serviceInfo", "/signin", "/signup", "/admin/signup")
-                        .anonymous() // 회원가입, 로그인 접근 권한
-                        .requestMatchers("/member/withdraw-success").permitAll()
-                        .requestMatchers("/admin/dashboard").hasRole("ADMIN") // 관리자페이지 접근 권한
-                        .requestMatchers("/member/**", "/dashboard/**", "/api/dashboard/**",
-                                "/todos/**", "/companies/**", "/goals/**", "/images/profile/**","/api/ai/feedback").hasRole("USER") // 사용자페이지 접근 권한
-                        .anyRequest().authenticated()
-                )
-                .formLogin(form -> form
-                        .loginPage("/signin")
-                        .usernameParameter("email")
-                        .loginProcessingUrl("/member/signin")
-                        .successHandler(successHandler())
-                )
-                .rememberMe(rememberMe -> rememberMe
-                        .key(rememberMeKey).rememberMeParameter("remember-me")
-                        .userDetailsService(memberService)
-                )
-                .exceptionHandling(exception -> exception
-                        .accessDeniedHandler(accessDeniedHandler())
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/signin")
-                        .invalidateHttpSession(true) // 세션무효화
-                        .deleteCookies("JSESSIONID")  // 쿠키 삭제
-                        .permitAll()
-                );
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/css/**", "/js/**", "/img/**", "/api/member/exists/*",
+                    "/member/interests", "/member/withdraw-success").permitAll()
+                .requestMatchers("/", "/serviceInfo", "/signin", "/signup", "/admin/signup")
+                .anonymous() // 회원가입, 로그인 접근 권한
+                .requestMatchers(HttpMethod.POST, "member/signin", "/auth/signin").permitAll()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/member/**", "/dashboard/**", "/api/dashboard/**",
+                    "/todos/**", "/companies/**", "/goals/**", "/images/profile/**","/api/ai/feedback").hasRole("USER") // 사용자페이지 접근 권한
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(logoutFilter, JwtAuthenticationFilter.class)
+            .addFilterBefore(authExceptionFilter, JwtAuthenticationFilter.class);
+
         return http.build();
     }
 
@@ -107,9 +99,36 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(MemberService memberService) {
-        return memberService;
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, accessDeniedException) -> {
+            response.setStatus(403);
+            response.setContentType("application/json");
+            response.getWriter().write("{\"error\": \"Access denied\"}");
+        };
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOriginPatterns(Collections.singletonList(
+            // 8081 요청 허용 (우리의 경우는 이메일 발송 서버)
+            "http://localhost:8081"
+        ));
 
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST"));
+        corsConfig.setAllowedHeaders(Collections.singletonList("*"));
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder, UserDetailsServiceImpl userDetailsService) throws Exception {
+        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(userDetailsService)
+            .passwordEncoder(passwordEncoder);
+        return builder.build();
+    }
 }
