89f974595e32f4cfe41c042f66614a342c3e91be
imseohyeon <forestlight749@gmail.com>
2025-06-15 23:01:31 +0900

    (0615) feat: #3 JWT 기반 인증 기능 구현
    - JwtProvider 구현
    - AccessTokenDto, TokenResponseDto, TokenType 등 토큰 관련 클래스 추가
    - Principal, GrantType 등 인증 관련 기본 구성

---
+++ DevQuest-service/src/main/java/com/grepp/nbe563team04/infra/auth/token/code/TokenType.java
@@ -0,0 +1,5 @@
+package com.grepp.nbe563team04.infra.auth.token.code;
+
+public enum TokenType {
+    ACCESS_TOKEN, REFRESH_TOKEN, AUTH_SERVER_SESSION_ID
+}
--- DevQuest-service/src/main/java/com/grepp/nbe563team04/model/auth/domain/Principal.java
+++ DevQuest-service/src/main/java/com/grepp/nbe563team04/model/auth/domain/Principal.java
@@ -3,61 +3,30 @@ package com.grepp.nbe563team04.model.auth.domain;
 import com.grepp.nbe563team04.model.member.entity.Member;
 import java.util.Collection;
 import java.util.List;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
 
-public class Principal implements UserDetails {
+@Getter
+public class Principal extends User {
 
     private final Member member;
 
+    public Principal(String email, String password,
+        Collection<? extends GrantedAuthority> authorities) {
+        super(email, password, authorities);
+        this.member = null;
+    }
+
     public Principal(Member member) {
+        super(member.getEmail(), member.getPassword(),
+            List.of(new SimpleGrantedAuthority(member.getRole().name())));
         this.member = member;
     }
 
-    public Member getUser() {
-        return this.member;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        // 권한 목록 리턴 (예: ROLE_USER 등)
-        return List.of(() -> member.getRole().name());
-    }
-
-    public static Principal createPrincipal(Member member, List<SimpleGrantedAuthority> authorities) {
-        return new Principal(member);
-//        return new Principal(member.getEmail(), member.getPassword(), authorities);
-    }
-
-    @Override
-    public String getPassword() {
-        return member.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return member.getEmail(); // 혹은 ID
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
+    public static Principal createPrincipal(Member member,
+        List<SimpleGrantedAuthority> authorities){
+        return new Principal(member.getEmail(), member.getPassword(), authorities);
     }
 }
-
