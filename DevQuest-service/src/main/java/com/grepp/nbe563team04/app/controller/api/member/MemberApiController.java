f91db63602983cc72e7489d6ddd7634da8dc211d
imseohyeon <forestlight749@gmail.com>
2025-06-17 23:04:44 +0900

    (0617) refactor: #4 이메일 인증 메일 관련 코드 삭제

--- DevQuest-service/src/main/java/com/grepp/nbe563team04/app/controller/api/member/MemberApiController.java
+++ DevQuest-service/src/main/java/com/grepp/nbe563team04/app/controller/api/member/MemberApiController.java
@@ -1,17 +1,12 @@
 package com.grepp.nbe563team04.app.controller.api.member;
 
-import com.grepp.nbe563team04.app.controller.web.member.payload.SignupRequest;
 import com.grepp.nbe563team04.infra.response.ApiResponse;
 import com.grepp.nbe563team04.model.member.MemberService;
-import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,16 +23,5 @@ public class MemberApiController {
         return ResponseEntity.ok(ApiResponse.success(
             memberService.isDuplicatedEmail(email)
         ));
-    }
-
-    @PostMapping("verify")
-    public ResponseEntity<ApiResponse<Void>> verify(
-        @Valid @RequestBody SignupRequest request,
-        HttpSession session) {
-
-        String token = session.getId();
-        session.setAttribute(token, request);
-        memberService.verify(token, request);
-        return ResponseEntity.ok(ApiResponse.noContent());
     }
 }
