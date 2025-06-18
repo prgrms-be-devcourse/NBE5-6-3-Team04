9598987c38335683ba910a9b1db11d2e89f2d348
imseohyeon <forestlight749@gmail.com>
2025-06-18 09:47:05 +0900

    (0618) fix: 로그인 실패 시, 에러 메시지 뜨지 않고, 조회 쿼리 무한으로 날리는 오류 해결

--- DevQuest-service/src/main/java/com/grepp/nbe563team04/app/controller/api/auth/AuthController.java
+++ DevQuest-service/src/main/java/com/grepp/nbe563team04/app/controller/api/auth/AuthController.java
@@ -6,6 +6,7 @@ import com.grepp.nbe563team04.infra.auth.token.TokenCookieFactory;
 import com.grepp.nbe563team04.infra.auth.token.code.GrantType;
 import com.grepp.nbe563team04.infra.auth.token.code.TokenType;
 import com.grepp.nbe563team04.infra.response.ApiResponse;
+import com.grepp.nbe563team04.infra.response.ResponseCode;
 import com.grepp.nbe563team04.model.auth.AuthService;
 import com.grepp.nbe563team04.model.auth.token.dto.TokenResponseDto;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,7 +15,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +38,7 @@ public class AuthController {
     ) {
         try {
             TokenResponseDto dto = authService.signin(req.getEmail(), req.getPassword());
+
             ResponseCookie accessTokenCookie = TokenCookieFactory.create(TokenType.ACCESS_TOKEN.name(),
                 dto.getAccessToken(), dto.getAtExpiresIn());
             ResponseCookie refreshTokenCookie = TokenCookieFactory.create(TokenType.REFRESH_TOKEN.name(),
@@ -51,18 +55,18 @@ public class AuthController {
 
             return ResponseEntity.ok(ApiResponse.success(tokenResponse));
 
-        } catch (AuthenticationException e) {
-            // 어떤 인증 에러든 통일된 실패 응답 반환
+        } catch (BadCredentialsException e) {
             return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
-                .body(ApiResponse.error());
+                .body(ApiResponse.error(ResponseCode.INVALID_PASSWORD));
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(ApiResponse.error(ResponseCode.EMAIL_NOT_FOUND));
+        } catch (AuthenticationException e) {
+            return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(ApiResponse.error(ResponseCode.BAD_REQUEST));
         }
-    }
-
-    @PostMapping("logout")
-    public ResponseEntity<ApiResponse<Void>> logout(
-        HttpServletResponse response
-    ){
-        return ResponseEntity.ok(ApiResponse.noContent());
     }
 }
--- DevQuest-service/src/main/java/com/grepp/nbe563team04/model/auth/AuthService.java
+++ DevQuest-service/src/main/java/com/grepp/nbe563team04/model/auth/AuthService.java
@@ -7,12 +7,18 @@ import com.grepp.nbe563team04.model.auth.token.UserBlackListRepository;
 import com.grepp.nbe563team04.model.auth.token.dto.AccessTokenDto;
 import com.grepp.nbe563team04.model.auth.token.dto.TokenResponseDto;
 import com.grepp.nbe563team04.model.auth.token.entity.RefreshToken;
+import com.grepp.nbe563team04.model.member.MemberRepository;
+import com.grepp.nbe563team04.model.member.entity.Member;
+import io.jsonwebtoken.security.Password;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,12 +32,21 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final UserBlackListRepository userBlackListRepository;
     private final JwtProvider jwtProvider;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     /**
      * 일반 로그인 처리 (이메일/비밀번호 검증 후 토큰 발급)
      * 1. 사용자 인증 단계
      */
     public TokenResponseDto signin(String email, String password){
+        Member member = memberRepository.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("해당 이메일 없음"));
+
+        if (!passwordEncoder.matches(password, member.getPassword())) {
+            throw new BadCredentialsException("비밀번호 불일치");
+        }
+
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(email, password);
         // loadUserByUsername + password 검증 후 authentication 반환
         Authentication authentication = authenticationManager.authenticate(authToken);
